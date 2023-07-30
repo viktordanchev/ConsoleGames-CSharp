b6d62080d672811f6484601c527d15f3066c0726
VDanchev <viktordan315@gmail.com>
2023-07-31 00:07:24 +0300

    Update

---
+++ Snake/за малкия/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace за_малкия
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 23 };
+            List<int> list2 = list;
+            list2[0] = 32;
+            Console.WriteLine(string.Join(", ", list)); //1, 23 
+            Console.WriteLine(string.Join(", ", list2)); //32, 23 
+        }
+    }
+}
