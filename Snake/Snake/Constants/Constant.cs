0d0daeb00ef3f0ff1a28820145208445399e23db
VDanchev <viktordan315@gmail.com>
2023-12-07 18:47:38 +0200

    Updated

--- Snake/Snake/Constants/Constant.cs
+++ Snake/Snake/Constants/Constant.cs
@@ -2,9 +2,13 @@
 {
     public class Constant
     {
+        //Board dimensions
         public static int RowIndex = 20;
         public static int ColumnIndex = 40;
+
+        //Snake Length
         public static int SnakeStartLength = 6;
+
         public static int ThreadSleep = 150;
     }
 }
